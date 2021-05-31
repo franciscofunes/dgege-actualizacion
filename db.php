6b9837fc68335fc0acf0f57a03d62ab910a065f8
Francisco Funes <francisco.funes@educacion.local>
2021-05-31 15:45:27 -0300

    external db

--- db.php
+++ db.php
@@ -2,7 +2,11 @@
 
 $db = new Mysqli;
 
-$db-> connect('localhost','root', '','base_primaria');
+//local host
+//$db-> connect('localhost','root', '','base_primaria');
+
+//db remote host
+$db-> connect('remotemysql.com','DwUV5JYpGG', 'RFuMENqx7Y','DwUV5JYpGG');
 
 if(!$db){
     echo "error database connection";
