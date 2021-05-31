8e497efde00f9a921794a4ff72e033c3bd12e878
Francisco Funes <francisco.funes@educacion.local>
2021-05-31 16:41:33 -0300

    db remote

--- db.php
+++ db.php
@@ -3,10 +3,10 @@
 $db = new Mysqli;
 
 //local host
-$db-> connect('localhost','root', '','base_primaria');
+//$db-> connect('localhost','root', '','base_primaria');
 
 //db remote host
-//$db-> connect('remotemysql.com','DwUV5JYpGG', 'RFuMENqx7Y','DwUV5JYpGG');
+$db-> connect('remotemysql.com','DwUV5JYpGG', 'RFuMENqx7Y','DwUV5JYpGG');
 
 if(!$db){
     echo "error database connection";
